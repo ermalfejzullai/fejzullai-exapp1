4eff04c4c6c1e804684c8274671e8e5a7fcce321
ermalfejzullai <ermalfejzullai@gmail.com>
2026-01-17 18:21:15 +0100

    second commit

--- src/renderer/src/pages/Home.tsx
+++ src/renderer/src/pages/Home.tsx
@@ -8,9 +8,8 @@ import { Button } from '../components/ui/button';
 import { ExchangeRate } from '@shared/types';
 import { useAuthStore } from '../store/useAuthStore';
 import { Plus, Trash2, Printer } from 'lucide-react';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from '../components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '../components/ui/dialog';
 import { Invoice } from '../components/Invoice';
-import { createRoot } from 'react-dom/client';
 
 export function Home() {
   const [rates, setRates] = useState<ExchangeRate[]>([]);
